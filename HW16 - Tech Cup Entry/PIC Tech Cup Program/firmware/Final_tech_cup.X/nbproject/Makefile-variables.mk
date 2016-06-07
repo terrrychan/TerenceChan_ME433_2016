435581164988645a8f3aca5cf75b16e819d3f65b
terrrychan <terencechan2016@u.northwestern.edu>
2016-06-06 22:50:41 -0500

    Robot with android app and PIC program working! Everything uploaded

---
+++ HW16 - Tech Cup Entry/PIC Tech Cup Program/firmware/Final_tech_cup.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mx795_pim_e16_int_dyn configuration
+CND_ARTIFACT_DIR_pic32mx795_pim_e16_int_dyn=dist/pic32mx795_pim_e16_int_dyn/production
+CND_ARTIFACT_NAME_pic32mx795_pim_e16_int_dyn=Final_tech_cup.X.production.hex
+CND_ARTIFACT_PATH_pic32mx795_pim_e16_int_dyn=dist/pic32mx795_pim_e16_int_dyn/production/Final_tech_cup.X.production.hex
+CND_PACKAGE_DIR_pic32mx795_pim_e16_int_dyn=${CND_DISTDIR}/pic32mx795_pim_e16_int_dyn/package
+CND_PACKAGE_NAME_pic32mx795_pim_e16_int_dyn=finaltechcup.x.tar
+CND_PACKAGE_PATH_pic32mx795_pim_e16_int_dyn=${CND_DISTDIR}/pic32mx795_pim_e16_int_dyn/package/finaltechcup.x.tar
+# pic32mz_ef_sk_int_dyn configuration
+CND_ARTIFACT_DIR_pic32mz_ef_sk_int_dyn=dist/pic32mz_ef_sk_int_dyn/production
+CND_ARTIFACT_NAME_pic32mz_ef_sk_int_dyn=Final_tech_cup.X.production.hex
+CND_ARTIFACT_PATH_pic32mz_ef_sk_int_dyn=dist/pic32mz_ef_sk_int_dyn/production/Final_tech_cup.X.production.hex
+CND_PACKAGE_DIR_pic32mz_ef_sk_int_dyn=${CND_DISTDIR}/pic32mz_ef_sk_int_dyn/package
+CND_PACKAGE_NAME_pic32mz_ef_sk_int_dyn=finaltechcup.x.tar
+CND_PACKAGE_PATH_pic32mz_ef_sk_int_dyn=${CND_DISTDIR}/pic32mz_ef_sk_int_dyn/package/finaltechcup.x.tar
